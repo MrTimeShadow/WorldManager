2bada4f224f896cef489573a67401f52a4780bd2
MrTimeShadow <timeshadowmc@gmail.com>
2013-03-23 13:43:16 +0100

    Some Changes

--- src/WorldManager.java
+++ src/WorldManager.java
@@ -8,7 +8,8 @@ public class WorldManager extends Plugin {
 	public static final Logger mclogger = Logger.getLogger("Minecraft");
 	
 	private WMCommandListener commandListener = new WMCommandListener();
-	private WMHookListener hookListener = new WMHookListener();
+	private WMPlayerListener playerListener = new WMPlayerListener();
+	private WMBlockListener blockListener = new WMBlockListener();
 	
 	@Override
 	/**
@@ -18,13 +19,10 @@ public class WorldManager extends Plugin {
 		this.setName("WorldManager v0.0.1 by MrTimeShadow");
 		mclogger.info("[WorldManager] Initializing WorldManager!");
 		PluginLoader loader = etc.getLoader();
-		loader.addListener(PluginLoader.Hook.COMMAND, commandListener, this, PluginListener.Priority.MEDIUM);
-		loader.addListener(PluginLoader.Hook.COMMAND_CHECK, commandListener, this, PluginListener.Priority.MEDIUM);
-		loader.addListener(PluginLoader.Hook.SERVERCOMMAND, commandListener, this, PluginListener.Priority.MEDIUM);
-		loader.addListener(PluginLoader.Hook.LOGIN, hookListener, this, PluginListener.Priority.MEDIUM);
-		loader.addListener(PluginLoader.Hook.DISCONNECT, hookListener, this, PluginListener.Priority.MEDIUM);
-		loader.addListener(PluginLoader.Hook.BAN, hookListener, this, PluginListener.Priority.MEDIUM);
-		loader.addListener(PluginLoader.Hook.KICK, hookListener, this, PluginListener.Priority.MEDIUM);
+		
+		this.addCommandListeners(loader);
+		this.addPlayerListeners(loader);
+		this.addBlockListeners(loader);
 		
 		mclogger.info("[WorldManager] Successfully enabled WorldManager");
 		
@@ -48,6 +46,29 @@ public class WorldManager extends Plugin {
 		etc.getInstance().removeCommand("/wm"); //Removes the Command from the Help list
 		PlayerLocationsFile.getInstance().save();
 		mclogger.info("[WorldManager] Successfully disabled WorldManager!");
+	}
+	
+	
+	private void addCommandListeners(PluginLoader loader) {
+		loader.addListener(PluginLoader.Hook.COMMAND, commandListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.COMMAND_CHECK, commandListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.SERVERCOMMAND, commandListener, this, PluginListener.Priority.MEDIUM);
+	}
+	
+	private void addPlayerListeners(PluginLoader loader) {
+		loader.addListener(PluginLoader.Hook.LOGIN, playerListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.DISCONNECT, playerListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.BAN, playerListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.KICK, playerListener, this, PluginListener.Priority.MEDIUM);
+	}
+	
+	private void addBlockListeners(PluginLoader loader) {
+		loader.addListener(PluginLoader.Hook.BLOCK_BROKEN, blockListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.BLOCK_CREATED, blockListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.BLOCK_DESTROYED, blockListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.BLOCK_PHYSICS, blockListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.BLOCK_PLACE, blockListener, this, PluginListener.Priority.MEDIUM);
+		loader.addListener(PluginLoader.Hook.BLOCK_UPDATE, blockListener, this, PluginListener.Priority.MEDIUM);
 	}
 
 }
