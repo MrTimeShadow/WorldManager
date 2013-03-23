e00d292dd686f1ee7d479c26e59a371a804cc979
MrTimeShadow <timeshadowmc@gmail.com>
2013-03-23 22:07:18 +0100

    Added the Hooks in the Block Listener, atm it doesn't do anything

--- src/WMBlockListener.java
+++ src/WMBlockListener.java
@@ -1,5 +1,41 @@
 
 public class WMBlockListener extends PluginListener {
 
-
+	public boolean onBlockBreak(Player player, Block block) {
+		
+		
+		return false;
+	}
+	
+	public boolean onBlockCreate(Player player, Block blockPlaced, Block blockClicked, int itemInHand) {
+		
+		return false;
+	}
+	
+	public boolean onBlockDestroy(Player player, Block block) {
+		
+		return false;
+	}
+	
+	public boolean onBlockPhysics(Block block, boolean placed) {
+		
+		return false;
+	}
+	
+	public boolean onBlockPlace(Player player, Block blockPlaced, Block blockClicked, Item itemInHand) {
+		
+		return false;
+	}
+	
+	public void onBlockRightClicked(Player player, Block blockClicked, Item itemInHand) {
+		
+	}
+	
+	public boolean onBlockUpdate(Block block, int newBlockId) {
+		
+		return false;
+	}
+	
+	
+	
 }
